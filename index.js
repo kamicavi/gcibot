a7c33891b6eab2fa520475eb25adc75e309ab625
kamicavi <>
2021-04-27 18:30:58 +0000

    Added nice fallbacks for which channel to use

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
+const getDefaultChannel = (guild) => {
+  //try systemChannel
+  if(guild.systemChannel)
+    return guild.systemChannel
+  
+  //try general
+  const generalChannel = guild.channels.find(channel => channel.name === "general");
+  if (generalChannel)
+    return generalChannel;
+    
+  //last resort, find the first channel with perms
+  return guild.channels
+   .filter(c => c.type === "text" &&
+     c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
+   .sort((a, b) => a.position - b.position ||
+     Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
+   .first();
+}
+
 bot.on('guildMemberAdd', member => {
-  console.log(`${member} joined`);member.guild.systemChannel.send(`${member} gci ded`);
+  console.log(`${member} joined`);
+  getDefaultChannel(member.guild).send(`${member} gci ded`);
 });
